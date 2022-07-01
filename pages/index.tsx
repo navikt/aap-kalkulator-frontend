31da71d5a4f0d6f6bbd5e00f32757b9ce35b1218
Øydis Kind Refsum <oydiskindrefsum@ydiss-MacBook-Pro.local>
2022-07-01 13:35:30 +0200

    :sparkles: Lagt til aria-labels og startknapp

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,30 @@
 import type { NextPage } from 'next'
 import Sidebanner from "../components/sidebanner/Sidebanner";
 import {Calculator, FlowerBladeFallFilled, HeartBroken} from "@navikt/ds-icons";
-import {Heading} from "@navikt/ds-react";
+import {BodyLong, Button, Heading} from "@navikt/ds-react";
 import Divider from "../components/divider/Divider";
 
 const Home: NextPage = () => {
-  return (<>
-      <Sidebanner tittel="Beregning av AAP"/>
-      <div className="container mx-auto">
-          <section className="bg-component-background-light ">
-              <div className="flex flex-col items-center">
-                <Calculator className="h-14 w-14 my-4"/>
-                <Heading size="large" spacing>
-                    Hvor mye kan jeg få?
-                </Heading>
-                  <Divider/>
-              </div>
-          </section>
-      </div>
-  </>)
+    return (<>
+        <Sidebanner tittel="Beregning av AAP"/>
+        <div className="md:w-2/3 mx-auto">
+            <section className="bg-component-background-light mt-8 pb-4 px-6">
+                <div className="flex flex-col items-center">
+                    <Calculator className="h-14 w-14 my-4"/>
+                    <Heading level="2" size="large" spacing aria-label="Hvor mye kan jeg få?">
+                        Hvor mye kan jeg få?
+                    </Heading>
+                    <Divider/>
+                </div>
+                <div className="flex flex-col items-center mt-4">
+                    <BodyLong spacing>
+                        Lord Vader will provide us with the location of the Rebel fortress by the time this station is operational. We will then crush the Rebellion with one swift stroke. It looks like Sandpeople did this, all right. Look, here are Gaffi sticks, Bantha tracks.
+                    </BodyLong>
+                    <Button className="w-20" variant="primary" aria-label="Start">Start</Button>
+                </div>
+            </section>
+        </div>
+    </>)
 }
 
 export default Home
