77a39b39b9185765d4eac10fe2bf2ba2786aef6a
johanne <johanne.tronstad@nav.no>
2022-07-01 08:32:50 +0200

    Revert "wip graphql"
    This reverts commit 1694f45b5068a52c3df80a9d285441dab52a3360.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,8 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import {ApolloProvider} from "@apollo/client";
-import {useApollo} from "../lib/apollo";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const apolloclient = useApollo(pageProps)
-  return <ApolloProvider client={apolloclient}><Component {...pageProps} /></ApolloProvider>
+  return <Component {...pageProps} />
 }
 
 export default MyApp
