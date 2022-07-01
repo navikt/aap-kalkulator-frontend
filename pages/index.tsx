f5ec0f422519c09e974c2eb76d6c23caf2e1c77b
Niro Karunakaran <niro.karunakaran@nav.no>
2022-07-01 12:47:32 +0200

    :sparkles: la til divider

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,22 @@
 import type { NextPage } from 'next'
 import Sidebanner from "../components/sidebanner/Sidebanner";
+import {Calculator, FlowerBladeFallFilled, HeartBroken} from "@navikt/ds-icons";
+import {Heading} from "@navikt/ds-react";
+import Divider from "../components/divider/Divider";
 
 const Home: NextPage = () => {
   return (<>
       <Sidebanner tittel="Beregning av AAP"/>
-      <div className="container mx-auto bg-component-background-light">
-          hello
+      <div className="container mx-auto">
+          <section className="bg-component-background-light ">
+              <div className="flex flex-col items-center">
+                <Calculator className="h-14 w-14 my-4"/>
+                <Heading size="large" spacing>
+                    Hvor mye kan jeg få?
+                </Heading>
+                  <Divider/>
+              </div>
+          </section>
       </div>
   </>)
 }
