e1494dfffde565a12194502a00464eaafb8f1d17
Niro Karunakaran <niro.karunakaran@nav.no>
2022-07-04 10:36:02 +0200

    :label: inntektsform interface

--- pages/steg/[steg].tsx
+++ pages/steg/[steg].tsx
@@ -1,17 +1,24 @@
 import { useRouter } from "next/router"
 import Container from "../../components/container/Container"
 import { Button, TextField } from "@navikt/ds-react"
+import React from "react"
+
+interface InntektsForm extends HTMLFormElement{
+    readonly inntekt1: HTMLInputElement,
+    readonly inntekt2: HTMLInputElement,
+    readonly inntekt3: HTMLInputElement
+}
 
 const Steg = () => {
     const router = useRouter()
     const { steg } = router.query
-    const handleSubmit = async (event) => {
+    const handleSubmit = async (event: React.FormEvent<InntektsForm>) => {
         event.preventDefault()
 
         const data = {
-            inntekt1: parseFloat(event.target.inntekt1.value),
-            inntekt2: parseFloat(event.target.inntekt2.value),
-            inntekt3: parseFloat(event.target.inntekt3.value),
+            inntekt1: parseFloat(event.currentTarget.inntekt1.value),
+            inntekt2: parseFloat(event.currentTarget.inntekt2.value),
+            inntekt3: parseFloat(event.currentTarget.inntekt3.value),
         }
 
         const endpoint = "http://0.0.0.0:8080/beregning"
