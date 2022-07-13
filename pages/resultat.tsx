3ba6407ae12fed812b81f2abb0b44f4e23dc79d0
johanne <johanne.tronstad@nav.no>
2022-07-13 10:45:05 +0200

    :lipstick: endret layout resultatside

--- pages/resultat.tsx
+++ pages/resultat.tsx
@@ -1,17 +1,26 @@
-import React, { useContext, useEffect } from "react"
+import React, { useContext, useEffect, useState } from "react"
 import { ResultState, State } from "./_app"
 import { useRouter } from "next/router"
 import { NextPage } from "next"
 import ResultContainer from "../components/container/ResultContainer"
-import { Alert, BodyShort, Heading, Link, ReadMore } from "@navikt/ds-react"
+import {
+    Alert,
+    BodyShort,
+    Heading,
+    Link,
+    ReadMore,
+    ToggleGroup,
+} from "@navikt/ds-react"
 import Image from "next/image"
 
 const Resultat: NextPage = () => {
     const { resultat, setResultat } = useContext(ResultState)
     const { state, setState } = useContext(State)
     const router = useRouter()
+    const [value, setValue] = useState("14dag")
 
     const gjennomsnitt = (state.inntekt1 + state.inntekt2 + state.inntekt3) / 3
+    const pengeStyling = "flex justify-center"
 
     useEffect(() => {
         //event.preventDefault()
@@ -49,63 +58,65 @@ const Resultat: NextPage = () => {
             >
                 Dette kan du få:
             </Heading>
-            <div className="grid md:grid-cols-3 md:grid-rows-1 gap-2 grid-cols-1 pb-4">
-                <ResultContainer>
-                    <Heading
-                        className="flex justify-center"
-                        size="small"
-                        spacing
-                        aria-label="Din utbetaling hver 14. dag"
-                    >
-                        Årlig
-                    </Heading>
-                    <div className="flex justify-center text-xl">
-                        {resultat.resultat} kr
-                    </div>
-                </ResultContainer>
-                <ResultContainer>
-                    <Heading
-                        className="flex justify-center"
-                        size="small"
-                        spacing
-                        aria-label="Din utbetaling hver 14. dag"
-                    >
+            <div className="grid grid-cols-2 mb-4">
+                <ToggleGroup
+                    onChange={(x) => setValue(x)}
+                    value={value}
+                    size="medium"
+                >
+                    <ToggleGroup.Item value="14dag">
                         Hver 14. dag
-                    </Heading>
-                    <div className="flex justify-center text-xl">
-                        {perDag * 10} kr
-                    </div>
-                </ResultContainer>
-                <ResultContainer>
-                    <Heading
-                        className="flex justify-center"
-                        size="small"
-                        spacing
-                        aria-label="Din utbetaling hver 14. dag"
-                    >
-                        Daglig
-                    </Heading>
-                    <div className="flex justify-center text-xl">
-                        {perDag} kr
-                    </div>
-                </ResultContainer>
+                    </ToggleGroup.Item>
+                    <ToggleGroup.Item value="arlig">Årlig</ToggleGroup.Item>
+                </ToggleGroup>
+                {value == "14dag" && (
+                    <p className="flex text-5xl justify-center">
+                        {(perDag * 10).toLocaleString().replace(",", " ")} kr
+                    </p>
+                )}
+                {value == "arlig" && (
+                    <p className="flex text-5xl justify-center">
+                        {resultat.resultat.toLocaleString().replace(",", " ")}{" "}
+                        kr
+                    </p>
+                )}
             </div>
-
-            <ReadMore size="small" header="Hvorfor får jeg denne summen?">
-                {" "}
-                Du har oppgitt at du har {gjennomsnitt} i gjennomsnittsinntekt
-                de siste tre årene og {state.inntekt1} i inntekt siste året før
-                du ble sykemeldt. Siden{" "}
-                {Math.max(gjennomsnitt, state.inntekt1) == gjennomsnitt
-                    ? "gjennomsnittsinntekten"
-                    : "inntekten året før du ble sykemeldt"}{" "}
-                er størst vil dette bli brukt i beregningen av din AAP. Ditt
-                opprinnelige grunnlag før barn og arbeid er «dette» kr. Da er
-                din ytelse (det du får utbetalt) x*66% kr Siden du jobber{" "}
-                {state.arbeidsgrad} vil du bli trukket z% i utbetalt AAP Siden
-                du har a barn under 18 år, vil du få et tillegg på 27*a kr. Din
-                AAP vil være «penger» i året
-            </ReadMore>
+            <div className="pb-4">
+                <ReadMore size="small" header="Hvorfor får jeg denne summen?">
+                    {" "}
+                    <ul className=" space-y-4">
+                        <li>
+                            Du har oppgitt at du har {gjennomsnitt} i
+                            gjennomsnittsinntekt de siste tre årene og{" "}
+                            {state.inntekt1} i inntekt siste året før du ble
+                            sykemeldt. Siden{" "}
+                            {Math.max(gjennomsnitt, state.inntekt1) ==
+                            gjennomsnitt
+                                ? "gjennomsnittsinntekten"
+                                : "inntekten året før du ble sykemeldt"}{" "}
+                            er størst vil dette bli brukt i beregningen av din
+                            AAP.{" "}
+                        </li>
+                        <li>
+                            {" "}
+                            Ditt opprinnelige grunnlag før barn og arbeid er
+                            «dette» kr. Da er din ytelse (det du får utbetalt)
+                            x*66% kr{" "}
+                        </li>
+                        <li>
+                            {" "}
+                            Siden du jobber {state.arbeidsgrad} vil du bli
+                            trukket z% i utbetalt AAP{" "}
+                        </li>
+                        <li>
+                            {" "}
+                            Siden du har a barn under 18 år, vil du få et
+                            tillegg på 27*a kr.{" "}
+                        </li>
+                        <li> Din AAP vil være «penger» i året</li>
+                    </ul>
+                </ReadMore>
+            </div>
             <div className="pt-4">
                 <Alert variant="info" size="small">
                     <p>
