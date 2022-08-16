aead6d425d96908410d80594c5b8b8b674ddac4f
vetleh <vetle.hollund@nav.no>
2022-08-16 12:46:02 +0200

    :bug: fjernet dobbel link i backlink

--- components/backlink/BackLink.tsx
+++ components/backlink/BackLink.tsx
@@ -1,15 +1,19 @@
 import { Link } from "@navikt/ds-react"
-import NextLink from "next/link"
 import { Back } from "@navikt/ds-icons"
+import { useRouter } from "next/router"
 
 const BackLink = ({ target, tekst }: { target: string; tekst?: string }) => {
+    const router = useRouter()
+
+    const onClick = async (e: React.MouseEvent) => {
+        e.preventDefault()
+        await router.push(target)
+    }
     return (
-        <NextLink href={target} className="mt-4 " passHref>
+        <Link as={"a"} href="#" className="mt-4 " onClick={onClick}>
             {" "}
-            <Link href={target} as={"a"}>
-                <Back /> {tekst ? tekst : "Tilbake"}
-            </Link>
-        </NextLink>
+            <Back /> {tekst ? tekst : "Tilbake"}
+        </Link>
     )
 }
 
