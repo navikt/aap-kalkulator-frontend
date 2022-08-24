e7081aa85aaf88c072127fd6f6bccdc01beccbdd
Halvor Bjørn <halvor.grizzly.bjorn@nav.no>
2022-08-24 14:10:07 +0200

    :wrench: Fjerner export som skrev over sentryConfig

--- next.config.js
+++ next.config.js
@@ -1,21 +1,18 @@
-
 /** @type {import('next').NextConfig} */
 
-const { withSentryConfig } = require('@sentry/nextjs');
+const { withSentryConfig } = require("@sentry/nextjs")
 
 const sentryWebpackPluginOptions = {
     silent: true,
-};
+}
 
 const nextConfig = {
     reactStrictMode: true,
-};
-
-if (process.env.ENABLE_SENTRY === "enabled") {
-    console.log('sentry enabled', process.env.ENABLE_SENTRY);
-    module.exports = withSentryConfig(nextConfig, sentryWebpackPluginOptions);
-} else {
-    module.exports = nextConfig;
 }
 
-module.exports = nextConfig
+if (process.env.ENABLE_SENTRY === "enabled") {
+    console.log("sentry enabled")
+    module.exports = withSentryConfig(nextConfig, sentryWebpackPluginOptions)
+} else {
+    module.exports = nextConfig
+}
