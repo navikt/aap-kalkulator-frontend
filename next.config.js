63c0739460112958aca2185427e8e8f9312d396b
Niro Karunakaran <niro.karunakaran@nav.no>
2022-08-24 13:38:52 +0200

    :loud_sound: lagt til sentry init

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
+
 /** @type {import('next').NextConfig} */
+
+const { withSentryConfig } = require('@sentry/nextjs');
+
+const sentryWebpackPluginOptions = {
+    silent: true,
+};
+
 const nextConfig = {
     reactStrictMode: true,
+};
+
+if (process.env.ENABLE_SENTRY === "enabled") {
+    console.log('sentry enabled', process.env.ENABLE_SENTRY);
+    module.exports = withSentryConfig(nextConfig, sentryWebpackPluginOptions);
+} else {
+    module.exports = nextConfig;
 }
 
 module.exports = nextConfig
