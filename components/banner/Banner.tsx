ebe72de8762805575984d102c82c17ac0fbcbaea
vetleh <vetle.hollund@nav.no>
2022-10-11 15:58:59 +0200

    :art: startet på ny banner

--- components/banner/Banner.tsx
+++ components/banner/Banner.tsx
@@ -1,12 +1,15 @@
-import { Heading } from "@navikt/ds-react"
+import { BodyShort, Heading } from "@navikt/ds-react"
 import React from "react"
 
 const Banner = ({ title }: { title: string }) => {
     return (
-        <header className="flex flex-col items-center justify-center h-16 md:h-14 bg-[#CCF1D6] border-b-[#06893A] border-solid border-b-4 text-center ">
-            <Heading size="medium" level="1" aria-label={title}>
+        <header className="flex flex-col items-center justify-center h-16 md:h-24 bg-[#ffffff] border-b-[#0084aa] border-solid border-b-4 text-center ">
+            <Heading size="large" level="1" aria-label={title} spacing>
                 {title}
             </Heading>
+            <BodyShort spacing>
+                KALKULATOR | Oppdatert 11.oktober 2022
+            </BodyShort>
         </header>
     )
 }
--- components/container/Container.tsx
+++ components/container/Container.tsx
@@ -2,11 +2,13 @@ import { ReactNode } from "react"
 import Banner from "../banner/Banner"
 import Breadcrumbs from "../breadcrumbs/Breadcrumbs"
 import React from "react"
+import { useFeatureToggleIntl } from "../../hooks/useFeatureToggleIntl"
 
 const Container = ({ children }: { children: ReactNode }) => {
+    const { formatMessage } = useFeatureToggleIntl()
     return (
         <div className="min-h-container bg-canvas-background">
-            <Banner title="Beregning av arbeidsavklaringspenger" />
+            <Banner title={formatMessage("banner")} />
 
             <div role="main" className="md:w-3/5 lg:w-2/5 mx-auto pb-8">
                 <Breadcrumbs />
