8223f32b436692b1634d43ef14b549a3fa89188b
Niro Karunakaran <niro.karunakaran@nav.no>
2022-10-24 16:02:36 +0200

    :children_crossing: venstrestilt dato under heading

--- components/banner/Banner.tsx
+++ components/banner/Banner.tsx
@@ -6,7 +6,7 @@ import Image from "next/image"
 const Banner = ({ updated }: { updated: string }) => {
     const { formatMessage } = useFeatureToggleIntl()
     return (
-        <header className="flex flex-col items-center justify-center h-auto bg-[#ffffff] border-b-[#0084aa] border-solid border-b-4 text-center ">
+        <header className="flex flex-col h-auto bg-[#ffffff] border-b-[#0084aa] border-solid border-b-4 text-center ">
             <div className="flex flex-row items-center justify-center h-full space-x-4 mr-4">
                 <div className="flex items-center ml-4 invisible md:visible">
                     <Image
@@ -24,7 +24,7 @@ const Banner = ({ updated }: { updated: string }) => {
                     {formatMessage("banner.title")}
                 </Heading>
             </div>
-            <div className="flex flex-row space-x-2 pb-2">
+            <div className="flex flex-row md:w-4/6 lg:w-2/5 mx-auto space-x-2 py-3 md:pl-24">
                 <p className="text-sm">{formatMessage("banner.description")}</p>
                 <p className="text-gray-400 text-sm">
                     {formatMessage("banner.updated")}
