b9014181d8ca6d668be465f953a95c38ec0bd31b
vetleh <vetle.hollund@nav.no>
2022-12-02 13:22:13 +0100

    :fire: fjernet screenshots i playwright test

--- playwright/WCAG.spec.ts
+++ playwright/WCAG.spec.ts
@@ -8,13 +8,11 @@ test('Gå igjennom kalkulatoren og sjekk WCAG', async ({ page }) => {
     await page.goto('http://localhost:3000/aap/kalkulator');
 
     let wcagRes = await checkWcag(page);
-    await page.screenshot({ path: `./screenshots/${Date.now()}.png` });
     await expect(wcagRes.violations).toEqual([]);
     await page.getByRole('button', { name: 'Start' }).click()
 
     await expect(page.getByRole('heading', { name: 'Helse' })).toBeVisible()
     wcagRes = await checkWcag(page);
-    await page.screenshot({ path: `./screenshots/${Date.now()}.png` });
     await expect(wcagRes.violations).toEqual([]);
 
     await page.getByLabel('Nei').click()
@@ -23,20 +21,16 @@ test('Gå igjennom kalkulatoren og sjekk WCAG', async ({ page }) => {
 
     await expect(page.getByRole('heading', { name: 'Inntekt' })).toBeVisible()
     wcagRes = await checkWcag(page);
-    await page.screenshot({ path: `./screenshots/${Date.now()}.png` });
     await expect(wcagRes.violations).toEqual([]);
     await page.getByLabel('Nei').click()
     await page.getByRole('button', { name: 'Gå videre' }).click()
 
     await expect(page.getByRole('heading', { name: 'Barn' })).toBeVisible()
     wcagRes = await checkWcag(page);
-    await page.screenshot({ path: `./screenshots/${Date.now()}.png` });
     await expect(wcagRes.violations).toEqual([]);
     await page.getByLabel('Nei').click()
     await page.getByRole('button', { name: 'Gå videre' }).click()
 
-    //screenshot
-    await page.screenshot({ path: `./screenshots/${Date.now()}.png` });
     await expect(page.getByText('Det ser ut som du kan få' )).toBeVisible()
     wcagRes = await checkWcag(page);
     await expect(wcagRes.violations).toEqual([]);
