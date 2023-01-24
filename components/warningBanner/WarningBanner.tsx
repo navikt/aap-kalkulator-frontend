d84f918b9dd6f6ae2196260a5bd3718cb6ad4ede
vetleh <vetle.hollund@nav.no>
2023-01-24 13:13:33 +0100

    :art: lysere farger -> høyere kontrast

--- components/warningBanner/WarningBanner.tsx
+++ components/warningBanner/WarningBanner.tsx
@@ -5,7 +5,7 @@ import { useFeatureToggleIntl } from "../../hooks/useFeatureToggleIntl"
 const WarningBanner = (text:{text:string}) => {
     const { formatMessage } = useFeatureToggleIntl()
     return (
-        <div className="justify-center bg-blue-300 flex gap-8 border-b-deepblue-400 border-b-4">
+        <div className="justify-center bg-blue-100 flex gap-8 border-b-deepblue-400 border-b-4">
                 <div className="">
                     <div className="flex py-3 items-baseline">
                         <BodyShort className="">
