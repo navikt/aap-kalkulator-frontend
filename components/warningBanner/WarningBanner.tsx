88fa61a05892805f8f8d0bc006c0d6528444a0e4
vetleh <vetle.hollund@nav.no>
2023-01-24 13:15:08 +0100

    :fire: trenger ikke intl format message

--- components/warningBanner/WarningBanner.tsx
+++ components/warningBanner/WarningBanner.tsx
@@ -1,15 +1,13 @@
-import { BodyShort, Heading } from "@navikt/ds-react"
+import { BodyShort } from "@navikt/ds-react"
 import React from "react"
-import { useFeatureToggleIntl } from "../../hooks/useFeatureToggleIntl"
 
 const WarningBanner = (text:{text:string}) => {
-    const { formatMessage } = useFeatureToggleIntl()
     return (
         <div className="justify-center bg-blue-100 flex gap-8 border-b-deepblue-400 border-b-4">
                 <div className="">
                     <div className="flex py-3 items-baseline">
                         <BodyShort className="">
-                            {formatMessage(text.text)}
+                            {text.text}
                         </BodyShort>
                     </div>
                 </div>
