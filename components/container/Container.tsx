57994b2185668eb565b376801073e11c7d225c77
VHollund <vetle.hollund@nav.no>
2023-01-27 08:39:27 +0100

    :sparkles: InfoBanner component

--- components/container/Container.tsx
+++ components/container/Container.tsx
@@ -3,14 +3,15 @@ import Banner from "../banner/Banner"
 import Breadcrumbs from "../breadcrumbs/Breadcrumbs"
 import React from "react"
 import { useFeatureToggleIntl } from "../../hooks/useFeatureToggleIntl"
+import WarningBanner from "../warningBanner/WarningBanner";
 
 const Container = ({ children }: { children: ReactNode }) => {
     const { formatMessage } = useFeatureToggleIntl()
     return (
         <div className="min-h-container bg-bg-subtle">
-            <Banner updated={"12.october 2022"} />
+            <Banner />
             <div className="px-4 md:px-12">
-            <main className="max-w-[900px] mx-auto pb-8">
+                <main className="max-w-[900px] mx-auto pb-8">
                 <div className="max-w-[600px]">
                 <Breadcrumbs />
                 <div
---
+++ components/warningBanner/WarningBanner.tsx
@@ -0,0 +1,18 @@
+import { Alert, BodyShort } from "@navikt/ds-react";
+import React from "react"
+
+const WarningBanner = (text:{text:string}) => {
+    return (
+        <div className="px-4 md:px-12">
+                <div className="max-w-[900px] mx-auto">
+                    <div className="pt-3 max-w-[600px] px-6 md:px-10">
+                        <Alert variant="info">
+                            {text.text}
+                        </Alert>
+                    </div>
+                </div>
+            </div>
+    )
+}
+
+export default WarningBanner
