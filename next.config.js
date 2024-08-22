f92cd956809e6d67c163c4f94ea5335190c46884
Tor Idland <tor.idland@nav.no>
2024-08-22 15:03:08 +0200

    legger til csp header

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
+const ssr = require('@navikt/nav-dekoratoren-moduler/ssr');
+
+const appDirectives = {
+    'connect-src': ["'self'"],
+    'font-src': ['https://fonts.gstatic.com'],
+    'object-src': ['none'],
+    'script-src-elem': ["'self'"],
+    'style-src-elem': ["'self'"],
+    'img-src': ["'self'", 'data:', 'blob:'],
+};
 
 const assetPrefix = process.env.ASSET_PREFIX ?? undefined
 
@@ -8,6 +18,21 @@ const nextConfig = {
     output: "standalone",
     assetPrefix:
         process.env.NODE_ENV === "production" ? assetPrefix : undefined,
+
+    async headers() {
+        const csp = await ssr.buildCspHeader(appDirectives, { env: process.env.DECORATOR_ENV ?? 'prod' });
+        return [
+            {
+                source: '/:path*',
+                headers: [
+                    {
+                        key: 'Content-Security-Policy',
+                        value: csp,
+                    },
+                ],
+            },
+        ];
+    },
     i18n: {
         locales: ["nb", "nn"],
         defaultLocale: "nb",
