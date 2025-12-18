09fcb524de6d156ab8aee876ce50be9e83a73a12
Øivind Stensrud <oivind.stensrud@nav.no>
2025-12-18 10:52:26 +0100

    Oppgraderer dekoratøren

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,4 @@
-import { DecoratorComponents, DecoratorEnvProps, fetchDecoratorReact } from '@navikt/nav-dekoratoren-moduler/ssr';
+import { DecoratorComponentsReact, DecoratorEnvProps, fetchDecoratorReact } from '@navikt/nav-dekoratoren-moduler/ssr';
 import Document, { DocumentContext, DocumentInitialProps, Head, Html, Main, NextScript } from 'next/document';
 import 'node-fetch';
 
@@ -13,7 +13,7 @@ const getDocumentParameter = (initialProps: DocumentInitialProps, name: string) 
 };
 
 interface Props {
-  Decorator: DecoratorComponents;
+  Decorator: DecoratorComponentsReact;
   language: string;
 }
 
@@ -27,7 +27,6 @@ class MyDocument extends Document<Props> {
         simple: false,
         chatbot: false,
         feedback: false,
-        urlLookupTable: false,
       },
     });
 
@@ -38,19 +37,17 @@ class MyDocument extends Document<Props> {
 
   render(): JSX.Element {
     const { Decorator, language } = this.props;
-    const showDecorator = true;
+
     return (
       <Html lang={language || 'no'}>
-        <Head>{showDecorator && <Decorator.Styles />}</Head>
+        <Head>
+          <Decorator.HeadAssets />
+        </Head>
         <body>
-          {showDecorator && <Decorator.Header />}
+          <Decorator.Header />
           <Main />
-          {showDecorator && (
-            <>
-              <Decorator.Footer />
-              <Decorator.Scripts />
-            </>
-          )}
+          <Decorator.Footer />
+          <Decorator.Scripts />
           <NextScript />
         </body>
       </Html>
